7d6716550358268d945427a3ea4d0c96b66319c9
morteza3123 <mkhedry3123@gmail.com>
2022-01-11 17:53:31 +0330

    add openzeppelin library for token and add tests for transfer

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,7 +2,19 @@
 
 pragma solidity >=0.5.0 <=0.9.0;
 
-contract Token {
-    string public name = 'Order-Dex';
+//import library
+import '../../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '../../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
+import '../../node_modules/@openzeppelin/contracts/access/Ownable.sol';
+import '../../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol';
 
+contract Token is ERC20, ERC20Burnable, Ownable, ERC20FlashMint {
+
+    constructor() ERC20('oxin', 'OXN') {
+        _mint(msg.sender, 1000000 * 10 **decimals());
+    }
+    
+    function mint(address to, uint256 amount) public onlyOwner{
+        _mint(to, amount);
+    }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,17 +1,67 @@
+const { Provider } = require('react-redux')
+
 const Token = artifacts.require('./Token')
 
 require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Token', (accounts) => {
+contract('Token', ([deployer, receiver]) => {
+    let name = 'oxin'
+    let symbol = 'OXN'
+    let decimals = '18'
+    let totalSupply = '1000000000000000000000000'
+    let token
+    beforeEach(async () => {
+         token = await Token.new()
+    })
 
     describe('deployment', () => {
         it('tracks the name', async () => {
-          const token = await Token.new()
           const result = await token.name()
-          result.should.equal('Order-Dex')
+          result.should.equal(name)
+        })
 
+        it('tracks the symbol', async () => {
+            const result = await token.symbol()
+            result.should.equal(symbol)
+        })
+
+        it('track the decimals', async () => {
+            const result = await token.decimals()
+            result.toString().should.equal(decimals)
+        })
+
+        it('tracks the total supply', async () => {
+            const result = await token.totalSupply()
+            result.toString().should.equal(totalSupply)
+        })
+
+        it('assign the total supply to the deployer', async () => {
+            const result = await token.balanceOf(deployer)
+            result.toString().should.equal(totalSupply)
+        })
+
+    })
+
+    describe('sending tokens', () => {
+        it('transfer token balances', async () => {
+            let balanceOf
+            //before transfer
+            balanceOf = await token.balanceOf(receiver)
+            console.log('receiver balance before transfer :', balanceOf.toString())
+            balanceOf = await token.balanceOf(deployer)
+            console.log('deployer balance before transfer :', balanceOf.toString())
+            
+            //transfer
+            await token.transfer(receiver, '100000000000000000000', {from : deployer})
+
+            //after transfer
+            balanceOf = await token.balanceOf(receiver)
+            console.log('receiver balance after transfer :', balanceOf.toString())
+            balanceOf = await token.balanceOf(deployer)
+            console.log('deployer balance after transfer :', balanceOf.toString())
+            
         })
     })
 })
